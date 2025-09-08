b78dba8dac3596d1c7279b8566546a08c41612db
Simon Duck <its.duck@gmail.com>
2025-09-08 17:23:26 +0100

    navbar highlighting fixed

--- src/scripts/navbarManager.js
+++ src/scripts/navbarManager.js
@@ -2,55 +2,59 @@ class Navbar extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
         <script>navbarContextHighlight.js</script>
+        <body>
             <header
       class="hidden landscape:flex justify-center text-center sticky top-0 z-51 "
     >
-      <nav
+      <nav id="navBar"
         class="fixed flex font-mono justify-center align-center bg-linear-to-r from-[#086375] to-[#78c3fb] text-[#ffffff] drop-shadow-xl/25 text-[calc(1vh+1vw*0.50)] lg:text-[calc(1vh+1vw*0.8)] xl:text-lg m-[5vh] w-90% md:w-[60%] rounded-[25px] md:rounded-[50px] overflow-hidden"
       >
         <ul
           class="flex flex-row text-center p-0 overflow-hidden list-none m-[8px] text-base xl:text-xl"
         >
-          <li
+          <li id="homeLink"
             class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw] text-center"
           >
-            <a href="index.html" class="block p=[0.1em] hover:text-[#086375]"
+            <a href="/index.html" class="navBarElement block p=[0.1em] hover:text-[#086375]"
               >Home</a
             >
           </li>
-          <li class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw] text-[#FFFF00]">
+          <li id="electronicsLink" class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw]">
             <a
-              href="electronics.html"
-              class="block p=[0.1em] hover:text-[#086375]"
+              href="/electronics.html"
+              class="navBarElement block p=[0.1em] hover:text-[#086375]"
               >Electronics</a
             >
           </li>
-          <li
+          <li 
             class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw] "
           >
-            <a href="soundart.html" class="block p=[0.1em] hover:text-[#086375]"
+            <a href="/soundart.html" id="SoundArtLink" class="navBarElement block p=[0.1em] hover:text-[#086375]"
               >Sound Art</a
             >
           </li>
-          <li class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw]">
-            <a
-              href="robotriddims.html"
-              class="block p=[0.1em] hover:text-[#086375]"
+          <li  class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw]">
+            <a 
+              href="/robotriddims.html"
+              id="RobotRiddimsLink"
+              class="navBarElement block p=[0.1em] hover:text-[#086375]"
               >Robot Riddims</a
             >
           </li>
 
-          <li class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw]">
+          <li class=" rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw]">
             <a
-              href="bambamsound.html"
-              class="block p=[0.1em] text-center hover:text-[#086375] "
+              href="/bambamsound.html"
+              id="bamBamSoundLink"
+              class="navBarElement block p=[0.1em] text-center hover:text-[#086375] "
               >Bam Bam Sound</a
             >
           </li>
           <li class="rounded-[50px] hover:bg-[rgba(244,116,59,0.6)] p-[0.5vw]">
             <a
               href="contactform.html"
-              class="block p=[0.1em] text-center hover:text-[#086375]"
+              id="contactLink"
+              class="navBarElement block p=[0.1em] text-center hover:text-[#086375]"
               >Contact</a
             >
           </li>
@@ -66,12 +70,12 @@ class Navbar extends HTMLElement {
     </label>
       <aside class="sidebar bg-[rgba(244,116,59,0.6)] landscape:hidden">
         <nav class="font-mono text-center font-size-[3em]">
-          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="index.html">Home</a></div>
-          <div class ="p=[0.5em] font-bold text-center m-5 text-[#FFFF00]"><a href="electronics.html">Electronics</a></div>
-          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="soundart.html">Sound Art</a></div>
-          <div class ="p=[0.5em] font-bold text-center m-5"><a href="robotriddims.html">Robot Riddims</a></div>
-          <div class ="p=[0.5em] font-bold text-center m-5"><a href="bambamsound.html">Bam Bam Sound</a></div>
-          <div class ="p=[0.5em] font-bold text-center m-5"><a href="contactform.html">Contact</a></div>
+          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="index.html" class="navBarElement">Home</a></div>
+          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="electronics.html" class="navBarElement">Electronics</a></div>
+          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="soundart.html" class="navBarElement">Sound Art</a></div>
+          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="robotriddims.html" class="navBarElement">Robot Riddims</a></div>
+          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="bambamsound.html" class="navBarElement">Bam Bam Sound</a></div>
+          <div class ="p=[0.5em] font-bold text-center m-5 "><a href="contactform.html" class="navBarElement">Contact</a></div>
         </nav>
 
 
@@ -82,3 +86,4 @@ class Navbar extends HTMLElement {
 };
 
 customElements.define('nav-bar',Navbar);
+
